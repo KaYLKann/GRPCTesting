bc859aaaef74ead426e367ef89e623b1465b568d
Vladimir <smitbbk777@gmail.com>
2020-01-21 23:42:25 +0300

    add check clear number

--- GrpcService/Services/CalcService.cs
+++ GrpcService/Services/CalcService.cs
@@ -17,13 +17,9 @@ namespace GrpcService {
         public override async Task<ExponentiationResult> Exec(ExponentiationRequest request, ServerCallContext context)
         {
             try {
-                if (request.Value < 1 ) {
-                    throw new ArgumentException($"value cannot be less 1");
-                }
-            
-                if (request.Value > 100 ) {
-                    throw new ArgumentException($"value cannot be more 100");
-                }
+                if (request.Value < 1 ) throw new ArgumentException($"value cannot be less 1");
+                if (request.Value > 50 ) throw new ArgumentException($"value cannot be more 100");
+                if ((request.Value % 2) == 0) throw new ArgumentException($"value cannot be {request.Value}");
                 
                 if (request.Operation == ExponentiationRequest.Types.Op.Square) {
                     return new ExponentiationResult( _exponentiationOperation.Calculate(request.Value, 3));
